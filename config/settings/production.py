c11605a430f2eb8d5d26b1d43f3ebac109eefe44
Manickaraj <manickaraj454@gmail.com>
2026-03-26 09:21:45 +0530

    fixed allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,26 @@
+import os
+
+import dj_database_url
+
 from .base import *
 
 DEBUG = False
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+DATABASES['default'] = dj_database_url.config(
+    default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / os.getenv('DJANGO_DB_NAME', 'db.sqlite3')}"),
+    conn_max_age=600,
+)
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_HSTS_SECONDS = 60 * 60 * 24 * 30
